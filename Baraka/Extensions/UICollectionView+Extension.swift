5efe2af67a744139e0ec9b9fc8745b88903235d1
Yazan <yazan@verda.ca>
2022-02-18 15:49:04 +0400

    constructs create list layout

--- Baraka/Extensions/UICollectionView+Extension.swift
+++ Baraka/Extensions/UICollectionView+Extension.swift
@@ -10,21 +10,64 @@ import UIKit
 
 extension UICollectionView {
     
-    private func createBasicListLayout() -> UICollectionViewLayout {
+    private func createListLayout() -> UICollectionViewLayout {
+        
+        let layout = UICollectionViewCompositionalLayout { [weak self] (sectionIndex: Int , environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            if sectionIndex == 0 {
+                return self?.createCapsulesLayout()
+            } else {
+                return self?.createArticlesLayout()
+            }
+        }
+        return layout
+    }
+    
+    private func createCapsulesLayout() -> NSCollectionLayoutSection {
         
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                              heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
       
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                              heightDimension: .absolute(44))
+                                               heightDimension: .absolute(74.0))
         
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
                                                          subitems: [item])
       
         let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
+        
+    }
+    
+    private func createArticlesLayout() -> NSCollectionLayoutSection {
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3),
+                                             heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+      
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                               heightDimension: .absolute(74.0))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
+                                                         subitems: [item])
+      
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 10
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+        
+        return section
+        
+    }
+}
 
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
+extension UICollectionViewCell: ReusableView {
+
+    static func register(for collectionView: UICollectionView)  {
+        let cellIdentifier = defaultReuseIdentifier
+        let cellNib = UINib(nibName: String(describing: self), bundle: nil)
+        collectionView.register(cellNib, forCellWithReuseIdentifier: cellIdentifier)
     }
 }
