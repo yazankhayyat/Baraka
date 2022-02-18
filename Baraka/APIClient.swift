1b695d999111d2f7bbd0355907330b4a8b6b4519
Yazan <yazan@verda.ca>
2022-02-18 11:48:47 +0400

    Adds APIclient with singleton to reuse anywhere

---
+++ Baraka/APIClient.swift
@@ -0,0 +1,25 @@
+//
+//  APIClient.swift
+//  Baraka
+//
+//  Created by Yazan Khayyat on 2022-02-18.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class APIClient {
+    static var shared = APIClient()
+    
+    let url = URL(string:"https://saurav.tech/NewsAPI/everything/cnn.json")!
+        
+    func getData() throws -> Observable<[Any]> {
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField:
+                      "Content-Type")
+        let helper = RxURLRequest(config: .default)
+        return helper.get(with: request)
+    }
+}
