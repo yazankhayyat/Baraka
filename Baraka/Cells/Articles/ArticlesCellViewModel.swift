56851ebd640ecd258aa96235ba70e1fd18f3b900
Yazan <yazan@verda.ca>
2022-02-18 21:06:37 +0400

    updates cellviewmodels to conform to hashable

--- Baraka/Cells/Articles/ArticlesCellViewModel.swift
+++ Baraka/Cells/Articles/ArticlesCellViewModel.swift
@@ -10,18 +10,27 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class ArticlesCellViewModel {
+class ArticlesCellViewModel: Hashable {
     
     let date: Driver<String>
     let title: Driver<String>
-    let articleImage: Driver<UIImage>
+    let articleImage: Driver<UIImage?>
 
-    let key: String
+    let id: String
     
-    init(with title: String, date: String, articleImage: UIImage, key: String) {
-       self.key = key
+    init(with title: String, date: String, articleImage: String, id: String) {
+        self.id = id
         self.title = Driver.just(title)
         self.date = Driver.just(date)
-        self.articleImage = Driver.just(articleImage)
+        self.articleImage = Driver.just(UIImage(named: articleImage))
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: ArticlesCellViewModel, rhs: ArticlesCellViewModel) -> Bool {
+        return lhs.id == rhs.id
     }
 }
+
--- Baraka/Cells/Capsule/CapsuleCellViewModel.swift
+++ Baraka/Cells/Capsule/CapsuleCellViewModel.swift
@@ -10,16 +10,24 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-class CapsuleCellViewModel {
+class CapsuleCellViewModel: Hashable {
     
     let symbol: Driver<String>
     let price: Driver<String>
 
-    let key: String
+    let id: String
     
-    init(with symbol: String, price: String, key: String) {
-       self.key = key
+    init(with symbol: String, price: String, id: String) {
+        self.id = id
         self.symbol = Driver.just(symbol)
         self.price = Driver.just(price)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: CapsuleCellViewModel, rhs: CapsuleCellViewModel) -> Bool {
+        return lhs.id == rhs.id
     }
 }
