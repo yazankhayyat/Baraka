e634f803bd7528024a37763c2d5cfe731ef7c239
Yazan <yazan@verda.ca>
2022-02-18 16:25:18 +0400

    registers cell classes

--- Baraka/ViewController.swift
+++ Baraka/ViewController.swift
@@ -6,12 +6,41 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+
+enum Section {
+    case section1
+    case section2
+    case section3
+}
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var articles: [Article] = []
+    let disposeBag = DisposeBag()
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    
+    let unknownError = "Something went wrong"
+    
+    let api = APIClient.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.register(UINib(nibName: ArticlesCell.defaultReuseIdentifier, bundle: nil), forCellWithReuseIdentifier: ArticlesCell.defaultReuseIdentifier)
+        
+        collectionView.register(UINib(nibName: CapsuleCell.defaultReuseIdentifier, bundle: nil), forCellWithReuseIdentifier: CapsuleCell.defaultReuseIdentifier)
+
+    }
+    
+    func showError(error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        showDetailViewController(alert, sender: self)
     }
 
     
