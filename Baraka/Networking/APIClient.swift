55fd84055bc9715044b02eefe1254427bae8ec92
Yazan <yazan@verda.ca>
2022-02-18 23:13:59 +0400

    tried get capsules

--- Baraka/Networking/APIClient.swift
+++ Baraka/Networking/APIClient.swift
@@ -14,6 +14,8 @@ class APIClient {
     
     let url = URL(string:"https://saurav.tech/NewsAPI/everything/cnn.json")!
         
+    let csvLink = "https://raw.githubusercontent.com/dsancov/TestData/main/stocks.csv"
+    
     func getData() -> Observable<Articles> {
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -22,4 +24,22 @@ class APIClient {
         let helper = RxURLRequest(config: .default)
         return helper.get(with: request)
     }
+    
+//    func getCapsules() -> Observable<[Capsule]> {
+//        var dataArray = [[String]]()
+//           let url = URL(string: csvLink)!
+//           do {
+//               let data = try Data(contentsOf: url)
+//               let dataEncoded = String(data: data, encoding: .utf8)
+//               if  let dataArr = dataEncoded?.components(separatedBy: "\r\n").map({ $0.components(separatedBy: ";") }) {
+//                   for line in dataArr {
+//                       dataArray.append(line)
+//                   }
+//               }
+//           } catch let jsonErr {
+//               print("\n Error reading CSV file: \n ", jsonErr)
+//           }
+//        return Observable.of(dataArray)
+//    }
+    
 }
